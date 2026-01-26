e5e41f0b7e09e68868a31b3ef849342b7d8574ba
U-RUSHC_NUC2\andy <andy@rushc.com>
2026-01-26 15:56:38 -0800

    park_tr_split -- moved multipliers around so only 2 needed
    Signed-off-by: U-RUSHC_NUC2\andy <andy@rushc.com>

---
+++ RTL/foc/park_tr_split.v
@@ -0,0 +1,89 @@
+
+//--------------------------------------------------------------------------------------------------------
+// 模块： park_tr
+// Type    : synthesizable
+// Standard: Verilog 2001 (IEEE1364-2001)
+// Split the multiplies over two cycles
+//--------------------------------------------------------------------------------------------------------
+
+module park_tr(
+    input  wire               rstn,
+    input  wire               clk,
+    input  wire        [11:0] psi,
+    input  wire               i_en,
+    input  wire signed [15:0] i_ialpha, i_ibeta,
+    output reg                o_en,
+    output reg  signed [15:0] o_id, o_iq
+);
+
+wire signed [15:0] sin_psi, cos_psi;  // -1~+1 is mapped to -16384~+16384
+
+reg               en_s1;
+reg signed [31:0] alpha_cos, alpha_sin, beta_cos, beta_sin;
+reg signed [31:0] beta_cos_d, beta_sin_d;
+   //
+   //Note that we use _d values (ie current values) for beta.
+   //alpha_ values computed on prior state
+   //
+   
+wire signed[31:0] ide = alpha_cos + beta_sin_d;
+wire signed[31:0] iqe = beta_cos_d  - alpha_sin;
+
+reg signed [15:0] i_ibeta_q;
+reg signed [15:0] sin_psi_q, cos_psi_q;
+
+   
+sincos u_sincos (
+    .rstn        ( rstn       ),
+    .clk         ( clk        ),
+    .i_en        ( 1'b1       ),
+    .i_theta     ( psi        ),
+    .o_en        (            ),
+    .o_sin       ( sin_psi    ),
+    .o_cos       ( cos_psi    )
+);
+
+always @ (posedge clk or negedge rstn)
+    if(~rstn) begin
+        {alpha_cos, alpha_sin, beta_cos, beta_sin,i_ibeta_q,sin_psi_q, cos_psi_q} <= 0;
+    end else begin
+       if (i_en)
+	 begin
+	    //multiply on state when i_en asserted
+            alpha_cos <= i_ialpha * cos_psi;
+            alpha_sin <= i_ialpha * sin_psi;
+	    //stash stuff for beta computation which is done on next
+	    //state
+	    sin_psi_q <= sin_psi;
+	    cos_psi_q <= cos_psi;
+	    i_ibeta_q <= i_ibeta;
+	 end
+    end
+
+   //combinational logic
+   //always executed on en_s1 state
+   //
+always @(*)
+  begin
+     if (en_s1) //multiply on en_s1 state (which is state after i_en)
+       begin
+	  beta_cos_d = i_ibeta_q  * cos_psi_q;
+	  beta_sin_d = i_ibeta_q  * sin_psi_q;
+       end
+end
+   
+   
+always @ (posedge clk or negedge rstn)
+    if(~rstn) begin
+        {o_en, o_id, o_iq,en_s1} <= 0;
+    end else begin
+       en_s1 <= i_en;
+       o_en <= en_s1;
+       if(en_s1) begin
+          o_id <= ide[31:16];
+          o_iq <= iqe[31:16];
+	  en_s1 <= 1'b0;
+       end
+    end
+
+endmodule
